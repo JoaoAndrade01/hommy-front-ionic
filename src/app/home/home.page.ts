9e8d765054fbc523d063827a92fa0241b4a35737
JoaoAndrade01 <joao.andrade@poli.ufrj.br>
2020-07-27 23:53:03 -0300

    + trab typescript

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { readdirSync } from 'fs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  republics = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.republics = [
+    {      
+      endereco: "Rua Zeca Pagodinho, Sao Paulo, SP",
+      tamanho: "85 m²",
+      quartos: "3",
+      valor: "R$350,00",
+    },
+    {
+      endereco: "Rua Nando Reis, Sao Paulo, SP",
+      tamanho: "95 m²",
+      quartos: "4",
+      valor: "R$885,00",
+    },
+    {
+      endereco: "Rua Gilberto Gil, Sao Paulo, SP",
+      tamanho: "73 m²",
+      quartos: "3",
+      valor: "R$465,00",
+    },
+    {
+      endereco: "Rua Bete Carvalho, Sao Paulo, SP",
+      tamanho: "56 m²",
+      quartos: "2",
+      valor: "R$620,00",
+    }
+    ];
+
   }
 
 }
